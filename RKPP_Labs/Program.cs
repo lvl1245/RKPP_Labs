f5de0a8427f65f4ecf0e22bea6d9e150eedfffcc
Keksik <lvl1245@yandex.by>
2023-10-29 21:43:54 +0300

    Переименовывание файлов и добавление проекта для лаб3

--- RKPP_Labs/Program.cs
+++ RKPP_Labs/Program.cs
@@ -8,32 +8,7 @@ namespace RKPP_Labs
     {
         static void Main(string[] args)
         {
-            try
-            {
-                TurningShop turningShop2 = new TurningShop();
-               
-                turningShop2.Print();
-
-                turningShop2.NumberOfWorkers = -15;
-               
-            }
-            catch (WorkshopNegativValueExeption)
-            {
-                Console.WriteLine("Исключение обработанно!");
-               
-            }
-
-
-            TurningShop turningShop4 = new TurningShop("Сергей Инвановчи", new List<string>()
-                                                                           {
-                                                                            "Изделие1",
-                                                                            "Изделие2",
-                                                                            "Изделие3"
-                                                                            },
-                                                                            "Токарный цех №4",17);        
-            turningShop4.Print();
-            turningShop4.CreateProduct();
-            
+            LabLoader.LoadLab_2();
 
         }
     }
---
+++ RKPP_Labs_Part_2/Form1.cs
@@ -0,0 +1,10 @@
+namespace RKPP_Labs_Part_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
